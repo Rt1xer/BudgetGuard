75d7a43446d94cd971984b90cefbbf603baca93d
Павел <yazooller@yandex.ru>
2025-05-06 18:29:20 +0300

    Fixed dynamic port Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -57,5 +57,5 @@ def init_defaults(user_id):
 if __name__ == '__main__':
     with app.app_context():
         db.create_all()  # Создаём все бинды
-        # можно вручную вызвать init_defaults() после регистрации пользователя
-    app.run(debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port)
